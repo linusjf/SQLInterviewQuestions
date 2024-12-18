9d9710c4c4c4198b0554a958c7b4ca542af1de01
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-18 21:34:22 +0530

    feat: import netflix csv

--- netflix.sql
+++ netflix.sql
@@ -24,10 +24,22 @@ update netflix_titles set listed_in = concat('[',replace(quote(listed_in),', ', 
 
 update netflix_titles set listed_in = replace(listed_in,'''''','\''\''');
 
+WITH cte(month, month_name) AS (VALUES
+  ('01', 'January'), ('02', 'February'), ('03', 'March'), ('04', 'April'), ('05', 'May'), ('06', 'June'),
+  ('07', 'July'), ('08', 'August'), ('09', 'September'), ('10', 'October'), ('11', 'November'), ('12', 'December')
+)
+SElECT SUBSTR(date_added, LENGTH(date_added) - 3, 4) || '-' || (SELECT c.month FROM cte c WHERE date_added LIKE '%' || c.month_name || '%' ) || '-' || printf('%02d', SUBSTR(date_added,LENGTH(date_added) - 7 , 2))  from netflix_titles LIMIT 15 offset 80;
+
+WITH ct(month, month_name) AS (VALUES
+  ('01', 'January'), ('02', 'February'), ('03', 'March'), ('04', 'April'), ('05', 'May'), ('06', 'June'),
+  ('07', 'July'), ('08', 'August'), ('09', 'September'), ('10', 'October'), ('11', 'November'), ('12', 'December')
+)
+UPDATE netflix_titles
+SET date_added = SUBSTR(date_added, LENGTH(date_added) - 3, 4) || '-' || (SELECT c.month FROM ct c WHERE date_added LIKE '%' || c.month_name || '%' ) || '-' || printf('%02d', SUBSTR(date_added,LENGTH(date_added) - 7 , 2));
+
+select date_added from netflix_titles limit 15 offset 80 ;
 select netflix_titles.show_id, listed_in as genre from netflix_titles where show_id = 's35';
 
 select netflix_titles.show_id, x.value as genre from netflix_titles, json_each(listed_in) as x;
 
 select count(*) as no_country_count from netflix_titles where country is NULL;
-
-select date_added from netflix_titles limit 5;
