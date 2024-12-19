92e755c120c41c7a50cbff990d59ba0c026d5d84
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-19 18:58:22 +0530

    feat: add beginner solution for employees

--- employeesoln.sql
+++ employeesoln.sql
@@ -1,5 +1,9 @@
 .print '---BEGINNER---';
 
+SELECT PaymentTier, COUNT(*) as employee_count from employee_information
+group by PaymentTier;
+
+SELECT Gender, ROUND(AVG(age), 2) as Average_Age from employee_information group by Gender;
 
 .print '---INTERMEDIATE---';
 
