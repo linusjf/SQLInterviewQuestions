5045683acba4d69d07479ac588dc57bc08c6e06f
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-19 15:18:02 +0530

    fix: format sql

--- employee.sql
+++ employee.sql
@@ -1,15 +1,16 @@
 -- Drop table if exists
 DROP TABLE IF EXISTS employee_information;
 -- Create table
-CREATE TABLE IF NOT EXISTS "employee_information"(         "Education" TEXT,
-"JoiningYear" INT,
-"City" TEXT,
-"PaymentTier" INT,
-"Age" INT,
-"Gender" TEXT,
-"EverBenched" TEXT,
-"ExperienceInCurrentDomain" INT,
-"LeaveOrNot" INT);
+CREATE TABLE IF NOT EXISTS "employee_information"(
+  "Education" TEXT,
+  "JoiningYear" INT,
+  "City" TEXT,
+  "PaymentTier" INT,
+  "Age" INT,
+  "Gender" TEXT,
+  "EverBenched" TEXT,
+  "ExperienceInCurrentDomain" INT,
+  "LeaveOrNot" INT);
 
 .mode csv
 
