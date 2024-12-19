e42fa5f854c413250799525a8168a4b870b0a1df
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-19 15:14:26 +0530

    feat: import employee.csv

---
+++ employee.sql
@@ -0,0 +1,16 @@
+-- Drop table if exists
+DROP TABLE IF EXISTS employee_information;
+-- Create table
+CREATE TABLE IF NOT EXISTS "employee_information"(         "Education" TEXT,
+"JoiningYear" INT,
+"City" TEXT,
+"PaymentTier" INT,
+"Age" INT,
+"Gender" TEXT,
+"EverBenched" TEXT,
+"ExperienceInCurrentDomain" INT,
+"LeaveOrNot" INT);
+
+.mode csv
+
+.import --skip 1 employee.csv employee_information
