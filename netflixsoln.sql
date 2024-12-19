0e6a4f1966939bb46aa6f062eff72e49e0968b9c
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-19 12:39:32 +0530

    feat: complete netflix assignment

--- netflixsoln.sql
+++ netflixsoln.sql
@@ -34,6 +34,32 @@ WITH genre_months AS (
         genres.value as genre,
   count(*) as monthly_count
   from netflix_titles, json_each(listed_in) as genres
+  where date_added is not NULL
   group by month, genre
+),
+prev_month_cte as (
+  SELECT
+  genre,
+  month,
+  monthly_count,
+  LAG(monthly_count) OVER (PARTITION BY genre ORDER by month) as prev_month_count
+  FROM genre_months
+),
+growth_rates as (
+  SELECT
+  genre,
+  month,
+  1.0 * (monthly_count - prev_month_count) / NULLIF(prev_month_count, 0) as growth_rate
+  FROM prev_month_cte
+),
+avg_growth_rates as (
+  SELECT
+  genre,
+  AVG(growth_rate) as avg_growth_rate
+  FROM growth_rates
+  WHERE growth_rate is NOT NULL
+  GROUP BY genre
 )
-select * from genre_months limit 5;
+select genre, ROUND(100.0 * avg_growth_rate, 2) as avg_growth_rate from avg_growth_rates
+order by avg_growth_rate desc
+limit 5;
