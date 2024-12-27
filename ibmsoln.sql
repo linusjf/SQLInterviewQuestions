f0e5d13fa421a0f9879dac4c115891718bf871cf
Linus Fernandes <linus.fernandes@gmail.com>
2024-12-27 20:27:03 +0530

    feat: add ibm problem

--- ibmsoln.sql
+++ ibmsoln.sql
@@ -6,8 +6,8 @@ WITH
     FROM
       user_purchases
     WHERE
-      day_name = 'Friday' and
-      CAST(strftime('%m', date) as INT) in (1,2,3)
+      day_name = 'Friday'
+      AND CAST(strftime('%m', date) AS INT) IN (1, 2, 3)
   )
 SELECT
   week_of_year,
