4f3003f678a172ac18a575337a0b9704f654956f
Linus Fernandes <linus.fernandes@gmail.com>
2025-01-05 21:07:10 +0530

    feat: add amazon user session problem

---
+++ amazonsessions.md
@@ -0,0 +1,13 @@
+Source: <https://www.linkedin.com/posts/im-nsk_sql-activity-7265197244021112832-bto5>
+
+## Amazon (Hard Level)
+
+Given the users' sessions logs on a particular day, calculate how many hours each user was active that day. Note: The session starts when state=1 and ends when state=0.
+
+🌀 Trust me, this one will surely challenge you...! Give it a try and share the output! 👇
+
+## 𝐒𝐜𝐡𝐞𝐦𝐚 𝐚𝐧𝐝 𝐃𝐚𝐭𝐚𝐬𝐞𝐭:
+
+CREATE TABLE customer_state_log (cust_id VARCHAR(10),state INT,timestamp TIME);
+
+INSERT INTO customer_state_log (cust_id, state, timestamp) VALUES('c001', 1, '07:00:00'),('c001', 0, '09:30:00'),('c001', 1, '12:00:00'),('c001', 0, '14:30:00'),('c002', 1, '08:00:00'),('c002', 0, '09:30:00'),('c002', 1, '11:00:00'),('c002', 0, '12:30:00'),('c002', 1, '15:00:00'),('c002', 0, '16:30:00'),('c003', 1, '09:00:00'),('c003', 0, '10:30:00'),('c004', 1, '10:00:00'),('c004', 0, '10:30:00'),('c004', 1, '14:00:00'),('c004', 0, '15:30:00'),('c005', 1, '10:00:00'),('c005', 0, '14:30:00'),('c005', 1, '15:30:00'),('c005', 0, '18:30:00');
---
+++ amazonsessions.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS customer_state_log;
+
+CREATE TABLE customer_state_log (cust_id VARCHAR(10), state INT, timestamp TIME);
+
+INSERT INTO
+  customer_state_log (cust_id, state, timestamp)
+VALUES
+  ('c001', 1, '07:00:00'),
+  ('c001', 0, '09:30:00'),
+  ('c001', 1, '12:00:00'),
+  ('c001', 0, '14:30:00'),
+  ('c002', 1, '08:00:00'),
+  ('c002', 0, '09:30:00'),
+  ('c002', 1, '11:00:00'),
+  ('c002', 0, '12:30:00'),
+  ('c002', 1, '15:00:00'),
+  ('c002', 0, '16:30:00'),
+  ('c003', 1, '09:00:00'),
+  ('c003', 0, '10:30:00'),
+  ('c004', 1, '10:00:00'),
+  ('c004', 0, '10:30:00'),
+  ('c004', 1, '14:00:00'),
+  ('c004', 0, '15:30:00'),
+  ('c005', 1, '10:00:00'),
+  ('c005', 0, '14:30:00'),
+  ('c005', 1, '15:30:00'),
+  ('c005', 0, '18:30:00');
+
+WITH
+  startend AS (
+    SELECT
+      cust_id,
+      state,
+      lag(timestamp) OVER (
+        PARTITION BY
+          cust_id
+        ORDER BY
+          timestamp
+      ) AS session_start,
+      timestamp AS session_end
+    FROM
+      customer_state_log
+  ),
+  filtered AS (
+    SELECT
+      cust_id,
+      session_start,
+      session_end
+    FROM
+      startend
+    WHERE
+      state = 0
+  ),
+  differences AS (
+    SELECT
+      cust_id,
+      strftime('%s', session_end) - strftime('%s', session_start) AS seconds_in_session
+    FROM
+      filtered
+  )
+SELECT
+  cust_id,
+  time(sum(seconds_in_session), 'unixepoch') AS user_hours
+FROM
+  differences
+GROUP BY
+  cust_id;
