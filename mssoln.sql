e3736d0cce9312b3e4b18d5621edf9437c396e7a
Linus Fernandes <linus.fernandes@gmail.com>
2025-01-10 05:37:15 +0530

    fix: fix references.qualifications errors

--- mssoln.sql
+++ mssoln.sql
@@ -1,6 +1,6 @@
 SELECT
   msp.title,
-  ROUND(budget * 1.0 / COUNT(emp_projs.emp_id), 2) AS budget_per_employee
+  ROUND(msp.budget * 1.0 / COUNT(emp_projs.emp_id), 2) AS budget_per_employee
 FROM
   ms_projects AS msp
   INNER JOIN ms_emp_projects AS emp_projs ON msp.id = emp_projs.project_id
